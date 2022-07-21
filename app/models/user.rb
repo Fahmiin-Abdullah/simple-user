d7c0137a868a4aba686bf25773c990cd71111edd
fahmiin <fahmiin@seedly.sg>
2022-07-21 22:53:18 +0800

    feat: added user model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, :last_name, :email, :password, presence: true
+
+  before_create :initialize_api_key
+
+  def initialize_api_key
+    return if api_key.present?
+
+    self.api_key = SecureRandom.hex(20)
+  end
+end
